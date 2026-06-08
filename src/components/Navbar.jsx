054f23cefa9ffc8fafef3835b0a59df3117c5af9
Munny <lalo.munro@gmail.com>
2026-06-08 09:25:25 -0600

    Menu-Hover-STate
    Diseño practicamente completo de los menus tipo dropdown para anime y manga, no son funcionales aun

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import { useState } from 'react'
 import styles from './Navbar.module.css'
 
-const NAV_LINKS = ['Anime', 'Manga', 'Comunidad', 'Temporada', 'Top']
+const NAV_LINKS = [
+  { name: 'Anime',
+    submenu: [
+      { name: 'Animes Populares' },
+      { name: 'Animes De Temporada' },
+      { name: 'Animes Mas Vistos' },
+      { name: 'Animes Menos Vistos' },
+    ]
+   },
+  { name: 'Manga',
+    submenu: [
+      { name: 'Mangas Populares' },
+      { name: 'Mangas De Temporada' },
+      { name: 'Mangas Mas Vistos' },
+      { name: 'Mangas Menos Vistos' },
+    ]
+   },
+]
 
 export default function Navbar() {
   const [searchFocused, setSearchFocused] = useState(false)
@@ -19,8 +36,19 @@ export default function Navbar() {
         {/* Links */}
         <ul className={`${styles.links} ${menuOpen ? styles.open : ''}`}>
           {NAV_LINKS.map(link => (
-            <li key={link}>
-              <a href="#" className={styles.link}>{link}</a>
+            <li key={link.name} className={styles.navItem}>
+              <a href="#" className={styles.link}>{link.name}</a>
+              {link.submenu && (
+                <ul className={styles.submenu}>
+                  {link.submenu.map(sublink => (
+                    <li key={sublink.name}>
+                      <a href="#" className={styles.submenuLink}>
+                        {sublink.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              )}
             </li>
           ))}
         </ul>
